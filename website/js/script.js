c597fee5d9e550fc43e5c7ea291d5b498d8b3827
Burak421 <57795979+Burak421@users.noreply.github.com>
2023-07-24 23:01:06 +0200

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -1,32 +1,42 @@
-(function($) {
-  // Function to handle the toggler click event
-  $(".navbar-toggler").click(function() {
-    // Toggle the "show" class on the collapsible div
-    $("#navbar1").toggleClass("show");
+// Function to handle the toggler click event
+function handleTogglerClick() {
+  $("#navbar1").toggleClass("show");
+  const icon = $(".navbar-toggler i");
+  icon.toggleClass("fa-bars fa-times");
+}
 
-    // Toggle the rotation of the toggle icon
-    $(this).find("i").toggleClass("fa-bars fa-times");
-  });
-})(jQuery);
+// Function to handle click outside of the collapsible div to close it
+function handleOutsideClick(event) {
+  if (!$(event.target).closest(".navbar").length) {
+    $("#navbar1").removeClass("show");
+    $(".navbar-toggler i").removeClass("fa-times").addClass("fa-bars");
+  }
+}
 
 // Check if the cookie banner should be displayed
 function shouldShowCookieBanner() {
-  return !localStorage.getItem('cookieConsent');
+  return !localStorage.getItem("cookieConsent");
 }
 
 // Close the cookie banner and store the user's consent
 function closeCookieBanner() {
-  document.getElementById('cookieBanner').style.display = 'none';
-  localStorage.setItem('cookieConsent', 'true');
+  document.getElementById("cookiePopup").style.display = "none";
+  localStorage.setItem("cookieConsent", "true");
 }
 
-// Event listener for the "Got it!" link
-document.getElementById('cookieClose').addEventListener('click', function (e) {
+// Event listener for the "Got it!" button
+document.getElementById("cookieClose").addEventListener("click", function (e) {
   e.preventDefault();
   closeCookieBanner();
 });
 
-// Display the cookie banner if user has not consented yet
+// Event listener for the toggler click
+$(".navbar-toggler").click(handleTogglerClick);
+
+// Event listener for click outside of the collapsible div
+$(document).click(handleOutsideClick);
+
+// Display the cookie banner if the user has not consented yet
 if (shouldShowCookieBanner()) {
-  document.getElementById('cookieBanner').style.display = 'block';
+  document.getElementById("cookiePopup").style.display = "block";
 }
