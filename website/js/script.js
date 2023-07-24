2cfa5045ae3fd61b8c9ae407e43f1bbbdcff8a32
Burak421 <57795979+Burak421@users.noreply.github.com>
2023-07-25 00:41:14 +0200

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -1,44 +1,19 @@
-// Function to handle the toggler click event
-function handleTogglerClick() {
-  $("#navbar1").toggleClass("show");
-  const icon = $(".navbar-toggler i");
-  icon.toggleClass("fa-bars fa-times");
-}
+(function($) {
+  $(".navbar-toggler").click(function() {
+    // Toggle the "show" class on the collapsible div
+    $("#navbar1").toggleClass("show");
 
-// Function to handle click outside of the collapsible div to close it
-function handleOutsideClick(event) {
-  if (!$(event.target).closest(".navbar").length) {
-    $("#navbar1").removeClass("show");
-    $(".navbar-toggler i").removeClass("fa-times").addClass("fa-bars");
-  }
-}
+    // Toggle the rotation of the toggle icon
+    $(this).find("i").toggleClass("fa-bars fa-times");
+  });
 
-// Check if the cookie banner should be displayed
-function shouldShowCookieBanner() {
-  return !localStorage.getItem("cookieConsent");
-}
+  // Handle click outside of the collapsible div to close it
+  $(document).click(function(event) {
+    if (!$(event.target).closest(".navbar").length) {
+      $("#navbar1").removeClass("show");
+      $(".navbar-toggler i").removeClass("fa-times").addClass("fa-bars");
+    }
+  });
+  
+})(jQuery);
 
-// Close the cookie banner and store the user's consent
-function closeCookieBanner() {
-  document.getElementById("cookiePopup").style.display = "none";
-  localStorage.setItem("cookieConsent", "true");
-}
-
-// Event listener for the "Got it!" button
-document.getElementById("cookieClose").addEventListener("click", function (e) {
-  e.preventDefault();
-  closeCookieBanner();
-});
-
-// Event listener for the toggler click
-$(".navbar-toggler").click(handleTogglerClick);
-
-// Event listener for click outside of the collapsible div
-$(document).click(handleOutsideClick);
-
-// Display the cookie banner if the user has not consented yet
-$(document).ready(function () {
-  if (shouldShowCookieBanner()) {
-    document.getElementById("cookiePopup").style.display = "block";
-  }
-});
