270d7b69a8f41d7937823a53257c9125d44871b5
Burak421 <57795979+Burak421@users.noreply.github.com>
2023-07-24 20:46:14 +0200

    Add files via upload

---
+++ website/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".navbar-toggler").click(function() {
+    // Toggle the "show" class on the collapsible div
+    $("#navbar1").toggleClass("show");
+
+    // Toggle the rotation of the toggle icon
+    $(this).find("i").toggleClass("fa-bars fa-times");
+  });
+
+  // Handle click outside of the collapsible div to close it
+  $(document).click(function(event) {
+    if (!$(event.target).closest(".navbar").length) {
+      $("#navbar1").removeClass("show");
+      $(".navbar-toggler i").removeClass("fa-times").addClass("fa-bars");
+    }
+  });
+});
