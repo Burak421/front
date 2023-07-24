3398dd650b6f6aa0660f8c42f84a3a3a75a3ba2c
Burak421 <57795979+Burak421@users.noreply.github.com>
2023-07-24 22:25:36 +0200

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -17,50 +17,24 @@
   });
 })(jQuery);
 
-// Function to set the cookie when the user clicks "Got It!"
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+// Check if the cookie banner should be displayed
+function shouldShowCookieBanner() {
+  return !localStorage.getItem('cookieConsent');
 }
 
-// Function to check if the user has already accepted the cookies
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(nameEQ) == 0) {
-      return c.substring(nameEQ.length, c.length);
-    }
-  }
-  return null;
+// Close the cookie banner and store the user's consent
+function closeCookieBanner() {
+  document.getElementById('cookieBanner').style.display = 'none';
+  localStorage.setItem('cookieConsent', 'true');
 }
 
-// Function to show or hide the cookie consent banner
-function checkCookieConsent() {
-  var cookieConsentBanner = document.getElementById('cookieConsentBanner');
-  if (!getCookie('cookie_consent')) {
-    cookieConsentBanner.style.display = 'block';
-  }
+// Event listener for the "Got it!" link
+document.getElementById('cookieClose').addEventListener('click', function (e) {
+  e.preventDefault();
+  closeCookieBanner();
+});
+
+// Display the cookie banner if user has not consented yet
+if (shouldShowCookieBanner()) {
+  document.getElementById('cookieBanner').style.display = 'block';
 }
-
-// Function to hide the cookie consent banner when the user clicks "Got It!"
-function acceptCookies() {
-  var cookieConsentBanner = document.getElementById('cookieConsentBanner');
-  setCookie('cookie_consent', 'true', 365); // Set the cookie to last for one year
-  cookieConsentBanner.style.display = 'none';
-}
-
-// Event listener for the "Got It!" button
-document.getElementById('cookieConsentButton').addEventListener('click', acceptCookies);
-
-// Check the cookie consent when the page loads
-window.addEventListener('load', checkCookieConsent);
