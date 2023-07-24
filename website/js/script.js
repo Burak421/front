6f7e0c18e3ef5c92a8d26358f593f1c75bb3156b
Burak421 <57795979+Burak421@users.noreply.github.com>
2023-07-24 22:18:27 +0200

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -16,3 +16,51 @@
     }
   });
 })(jQuery);
+
+// Function to set the cookie when the user clicks "Got It!"
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+// Function to check if the user has already accepted the cookies
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) == 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
+}
+
+// Function to show or hide the cookie consent banner
+function checkCookieConsent() {
+  var cookieConsentBanner = document.getElementById('cookieConsentBanner');
+  if (!getCookie('cookie_consent')) {
+    cookieConsentBanner.style.display = 'block';
+  }
+}
+
+// Function to hide the cookie consent banner when the user clicks "Got It!"
+function acceptCookies() {
+  var cookieConsentBanner = document.getElementById('cookieConsentBanner');
+  setCookie('cookie_consent', 'true', 365); // Set the cookie to last for one year
+  cookieConsentBanner.style.display = 'none';
+}
+
+// Event listener for the "Got It!" button
+document.getElementById('cookieConsentButton').addEventListener('click', acceptCookies);
+
+// Check the cookie consent when the page loads
+window.addEventListener('load', checkCookieConsent);
